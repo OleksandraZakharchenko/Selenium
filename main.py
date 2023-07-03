f0d4b6fe77f40151e97a485f2a80b9629e101c97
Oleksandra <102229324+OleksandraZakharchenko@users.noreply.github.com>
2023-07-03 10:12:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,41 +2,6 @@ from fastapi import FastAPI, BackgroundTasks, HTTPException
 from pydantic import BaseModel
 from extract import *
 import os
-
-
-SECRET = os.getenv("SECRET")
-
-#
-app = FastAPI()
-
-class Msg(BaseModel):
-    msg: str
-    secret: str
-
-@app.get("/")
-
-async def root():
-    return {"message": "Hello!"}
-
-
-@app.get("/homepage")
-async def demo_get():
-    driver=createDriver()
-
-    homepage = getGoogleHomepage(driver)
-    driver.close()
-    return homepage
-
-@app.post("/backgroundDemo")
-async def demo_post(inp: Msg, background_tasks: BackgroundTasks):
-    
-    background_tasks.add_task(doBackgroundTask, inp)
-    return {"message": "Success, background task started"}
-    
-from fastapi import FastAPI, BackgroundTasks, HTTPException
-from pydantic import BaseModel
-from extract import *
-import os
 import requests
 import subprocess
 
@@ -86,5 +51,3 @@ async def get_video_length(video_url: str):
     os.remove(temp_file)
 
     return {"video_length": video_length}
-
-
